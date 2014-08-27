a4a7ffeacc62f6ea02ae09bfec70ea27def9af54
karl anderson <karl@2600hz.com>
2014-08-27 15:47:49 -0400

    added most active times

--- klap/analyzers/klap-system.sh
+++ klap/analyzers/klap-system.sh
@@ -12,4 +12,5 @@ for FILE in $(listFiles $@); do
 	printStat "Unique PIDs" `cat $TMP_FILE | sort | uniq | wc -l`
 	zgrep -Eo "<[0-9]+\.[0-9]+\.[0-9]+>" $TMP_FILE | sort | uniq -c | sort -nr | head | printTable "Most Frequent PIDs"
 	zgrep -Eo "\|\w+:[0-9]+ " $TMP_FILE | cut -c2- | sort | uniq -c | sort -nr | head | printTable "Most Frequent Lines of Code"
+	zgrep -Eo "^\w+ \w+ [0-9]+:[0-9]+" "${FILE}" | sort | uniq -c | sort -nr | head | printTable "Most Active Times"
 done
