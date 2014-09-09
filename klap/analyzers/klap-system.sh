8dc54aadd2b5c59c1c9917e42b8fe1c38e4e6146
root <root@apps002.ewr.p.zswitch.net>
2014-09-09 19:56:33 +0000

    add OS PID check

--- klap/analyzers/klap-system.sh
+++ klap/analyzers/klap-system.sh
@@ -13,4 +13,5 @@ for FILE in $(listFiles $@); do
 	zgrep -Eo "<[0-9]+\.[0-9]+\.[0-9]+>" $TMP_FILE | sort | uniq -c | sort -nr | head | printTable "Most Frequent PIDs"
 	zgrep -Eo "\|\w+:[0-9]+ " $TMP_FILE | cut -c2- | sort | uniq -c | sort -nr | head | printTable "Most Frequent Lines of Code"
 	zgrep -Eo "^\w+ \w+ [0-9]+:[0-9]+" "${FILE}" | sort | uniq -c | sort -nr | head | printTable "Most Active Times"
+        zgrep -Eo "2600hz\[[0-9]+\]" "${FILE}" | sort | uniq | printTable "OS Processes"
 done
