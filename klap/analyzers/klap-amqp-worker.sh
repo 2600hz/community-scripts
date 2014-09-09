496621dec296f3d8335256de4e25a779ae2b798e
root <root@apps002.ewr.p.zswitch.net>
2014-09-09 20:23:53 +0000

    add failed publishes to amqp-worker stats

--- klap/analyzers/klap-amqp-worker.sh
+++ klap/analyzers/klap-amqp-worker.sh
@@ -23,6 +23,7 @@ for FILE in $(listFiles $@); do
 	printStat "Requests" $(countMatches "published request")
 	printStat "Unexpected Message" $(countMatches "received unexpected message")
 	printStat "Timeouts" $(countMatches "request timeout")
+	printStat "Failed Publishes" $(countMatches "failed to publish request")
 
 	zgrep -Eo " took [0-9]+ micro to return" $TMP_FILE | grep -Eo "[0-9]+" | normalize | sort -nr | uniq -c | printTable "Lookup Histogram"
 done
