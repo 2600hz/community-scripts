7a0565511ec4c1615fa36b4ca547b7e4c2813d1e
root <root@apps001.ord.p.zswitch.net>
2014-09-10 21:33:44 +0000

    couch_mgr klap

---
+++ klap/analyzers/klap-couch-mgr.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd `dirname $0`
+
+. ../klap-utils.sh
+
+for FILE in $(listFiles $@); do
+	createTmpFile "couch_util"
+
+	isTmpFileEmpty
+	if [ $? != 0 ]; then
+		continue
+	fi
+
+        statHeader
+
+	zgrep "unformatted error" $TMP_FILE | rev | cut -d " " -f 1 | rev | sort | uniq -c | sort -nr | head | printTable "Unformatted Errors"
+done
