2825f8c766c7366232fc6d3e52fd31cf73aa5de2
James Aimonetti <james@2600hz.org>
2015-01-20 22:04:54 -0800

    include _compact in uri

--- BigCouch/compact_commands.sh
+++ BigCouch/compact_commands.sh
@@ -2,4 +2,4 @@
 
 ## Replace the find command with whatever will print the full path of the .couch file. The output will be the curl command necessary to compact that shard.
 
-find /srv/db/shards/ -name "*201501*" | awk -F '/' 'BEGIN {OFS="%2F"} {print "curl -X POST -H \"Content-type:application/json\" localhost:5986/" $4, $5, $6, $7, $8, $9}' | sed -e 's/\.couch//g' > /tmp/compact.me
+find /srv/db/shards/ -name "*201501*" | awk -F '/' 'BEGIN {OFS="%2F"} {print "curl -X POST -H \"Content-type:application/json\" localhost:5986/" $4, $5, $6, $7, $8, $9 "/_compact"}' | sed -e 's/\.couch//g' > /tmp/compact.me
