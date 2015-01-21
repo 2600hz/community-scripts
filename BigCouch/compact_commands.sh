eaca8fa585d0de23f4fb254a2e61a6800d99002b
James Aimonetti <james@2600hz.org>
2015-01-20 22:01:50 -0800

    create compact commands

---
+++ BigCouch/compact_commands.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+## Replace the find command with whatever will print the full path of the .couch file. The output will be the curl command necessary to compact that shard.
+
+find /srv/db/shards/ -name "*201501*" | awk -F '/' 'BEGIN {OFS="%2F"} {print "curl -X POST -H \"Content-type:application/json\" localhost:5986/" $4, $5, $6, $7, $8, $9}' | sed -e 's/\.couch//g' > /tmp/compact.me
