8b1a34de83556a029ca7d59ab4afbe63020ab81b
root <root@debian.local>
2016-05-21 22:44:02 -0400

    Adding Kazoo 4 build script
    Adding git dependency

---
+++ buildKazoo4.sh
@@ -0,0 +1,17 @@
+sudo apt-get install git build-essential libxslt-dev \
+    zip unzip expat zlib1g-dev libssl-dev curl \
+    libncurses5-dev git-core libexpat1-dev \
+    htmldoc
+
+curl -O https://raw.githubusercontent.com/yrashk/kerl/master/kerl
+chmod a+x kerl
+mv kerl /usr/bin
+kerl list releases
+kerl build 18.2 r18.2 # this takes a while
+kerl install r18.2 /usr/lib/erlang
+. /usr/lib/erlang/activate
+
+cd /opt
+git clone https://github.com/2600Hz/kazoo.git
+cd kazoo
+make
