5c7dd7ac7ca2af22c0f4746008473315c9b7dcbb
sean wysor <sean@2600hz.com>
2019-03-12 11:55:33 -0700

    fixed an issue with sipify (#80)

--- FreeSWITCH/sipify.sh
+++ FreeSWITCH/sipify.sh
@@ -27,14 +27,14 @@ zgrep -EC 100 -e 'your_call_id|leg-2-call_id|leg-3-call-id' | \
 
  First grep is done with zgrep which will auto detect and unzip a compressed file
  or compressed piped stream. Grep is also way faster then awk for simple searches
- and the awk script is much more complex than a simple search. So grep
- is used before awk to efficiently filter a larg file to at most a few thousand
+ and the awk script is much more complex than a simple search. So grep 
+ is used before awk to efficiently filter a larg file to at most a few thousand 
  log lines are passed to sipify (awk). Since the SIP packet lines don't all have
  the call id on them this grep needs to output context (lines before and after a
- match), the -C 100 option. It uses -E for extended regex so the pipe symbol will
+ match), the -C 100 option. It uses -E for extended regex so the pipe symbol will 
  mean logical OR in the search string. That way you can specifiy multiple call ids.
  Finally the -e signifies what follows is the pattern match. This can be important
- because of the way it calls the normal grep internally and in somecases the
+ because of the way it calls the normal grep internally and in somecases the 
  call-id may have characters that confuse grep.
 
 ./sipify.sh | \
@@ -55,15 +55,15 @@ tee /tmp/ticket_num_`date +%b%d`_`hostname -f`.log
 
  This part is mostly personal preference. I like to use tee so it outputs to the
  file and screen so I can see that something was actually found without checking
- afterword the output file. The filename I like to have be something meaningful
+ afterword the output file. The filename I like to have be something meaningful 
  so make it have the ticket number, the month/day like Jan03 and the hostname.
-
- if your /tmp dir has lots of files, recordings and inbound faxes are put there,
+ 
+ if your /tmp dir has lots of files, recordings and inbound faxes are put there, 
  and your desktop SCP client wants to list all the files before you can find and
  copy it, make a dir like /tmp/log/ and put the log in it.
 
- NOTE pv is pipe verbose. it has the ability to throttle data passed through it
-   as well its other primary feature. Nice for preventing grep from overloading
+ NOTE pv is pipe verbose. it has the ability to throttle data passed through it 
+   as well its other primary feature. Nice for preventing grep from overloading  
    disk or CPU. Installed with yum install pv from epel repo.
 EOF
 }
@@ -71,33 +71,44 @@ EOF
 sipify(){
 awk '
 BEGIN{
-    I = 0
-    MSGFLDOFFSET = 0
-    ADDCALLIDAT = -1
-    CALLID = ""
+    PktLnCnt = 0
+    FldOfPktByte = 0
+    AddCallidAt = -1
+    CallID = ""
 }
-MSGFLDOFFSET == 0 && /send|recv/ && /(send|recv) [0-9]* bytes (to|from)/ {
-    # find the awk field that has the send or recv to auto support both FS direct
-    # logs and ones with stuff like date-time in front of packet data like syslog
+#{
+#another way to remove file name present from grepping multiple files
+#sub(/^[^ :]+:/, "")
+#}
+FldOfPktByte == 0 && /send|recv/ && /(send|recv) [0-9]* bytes (to|from)/ {
+    # find the awk field that has the send or recv to auto support FS direct
+    # logs and ones with stuff like date-time or log file name in front of packet 
+    # data like syslog and grep multiple files
     i = 0
-    while ( i++ <= NF && MSGFLDOFFSET == 0 ) {
-        if ( $i == "send" || $i == "recv" ) {
-            MSGFLDOFFSET = i
-        }
-    }
+    while ( i++ <= NF && FldOfPktByte == 0 )
+        if ( $i == "bytes" ) #send|recv not in own field when from grep multi file
+            FldOfPktByte = i
 }
-$MSGFLDOFFSET == "send" || $MSGFLDOFFSET == "recv" {
-    # another part to auto support syslog or FS direct
+$FldOfPktByte == "bytes" && AddCallidAt < 0 && /(send|recv) [0-9]* bytes (to|from)/ {
+    # found start of packet
     # find char pos to insert the call-id
-    t = $0
-    sub(/(send|recv) [0-9]* bytes (to|from).*/, "", t)
-    ADDCALLIDAT = length(t)
+    OffsetStr = $0
+    sub(/(send|recv) [0-9]* bytes (to|from).*/, "", OffsetStr)
+    AddCallidAt = length(OffsetStr) + 1
+    if ( $(FldOfPktByte - 2) ~ ".(send|recv)$" ) {
+        HeaderFldOffset = FldOfPktByte - 1
+    } else {
+       HeaderFldOffset = FldOfPktByte - 2
+    }
+    PktLines[PktLnCnt++] = $0
+    next
 }
-ADDCALLIDAT < 0 {
+AddCallidAt < 0 {
     # not in a sip packet dump print whole line as is
     print $0
+    next
 }
-ADDCALLIDAT >= 0 {
+AddCallidAt >= 0 {
     # in a sip packet dump
     if ( $0 == "--" || $0 ~ "[^-]--$" ) {
         #end of grep context, print the packet we have so far
@@ -108,41 +119,41 @@ ADDCALLIDAT >= 0 {
     if ( substr($0, (AddCallidAt - 1), 3) != "   " && $HeaderFldOffset != "" ) {
         # some log line mixed in the middle of the packet dump. Print ahead of packet dump.
         print $0
-        next
+        next  
     }
     # buffer line till end of packet
-    PACKET[I++] = $0
-    if ($MSGFLDOFFSET == "Call-ID:" || $MSGFLDOFFSET == "i:") {
+    PktLines[PktLnCnt++] = $0
+    if ($HeaderFldOffset == "Call-ID:" || $HeaderFldOffset == "i:") {
         #found packet call-id save for print insert
-        CALLID = $(MSGFLDOFFSET + 1)
+        CallID = $(HeaderFldOffset + 1)
+        next
     }
-    if (substr($MSGFLDOFFSET, 0, 5) == "-----") {
-        # packet delimiter
-        if(CALLID != ""){
-            # end of packet, print each line
-            for( i = 0; i < I ; i++ ){
-                print substr(PACKET[i], 0, ADDCALLIDAT) CALLID, substr(PACKET[i], (ADDCALLIDAT + 1))
-                delete PACKET[i]
-            }
-            I = 0
-            CALLID = ""
-            ADDCALLIDAT = -1
+    if (substr($HeaderFldOffset, 0, 5) == "-----" ) {
+        # packet delimiter or end of grep context
+        if(PktLnCnt > 2 ){
+            # end of packet (line 2 of packet also has "----")
+            printpacket()
         }
     }
 }
 END{
-    if ( CALLID != "" ){
-        #end of file but not end of packet and we have the call-id
-        for(i=0;i<I;i++){
-            print substr(PACKET[i], 0, ADDCALLIDAT) PRESP CALLID, substr(PACKET[i], (ADDCALLIDAT + 1))
-            #delete PACKET[i]
-        }
+    #end of file but maybe not end of packet and need to print the lines
+    printpacket()
+}
+function printpacket(){
+    for(i=0;i<PktLnCnt;i++){
+        print substr(PktLines[i], 0, AddCallidAt - 1) CallID, substr(PktLines[i], (AddCallidAt))
+        delete PktLines[i]
     }
+    PktLnCnt = 0
+    CallID = ""
+    AddCallidAt = -1
 }
 ' "$@"
 }
 if [[ ( -z "$@" || "$1" == "-h" || "$1" == "--help" ) && -t 0 ]] ; then
     usage
 else
-    sipify "$@" <&0
+     #sed 's/^[^ :]\+://' "$@" <&0 | sipify # strip file path from a multi file grep that puts the file/path in front of every log line. FYI: "grep --no-filename" will surpress the file name
+     sipify "$@" <&0
 fi
