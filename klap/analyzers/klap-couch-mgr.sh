d1df1d2439a7f01ed1dd038281018dd5680baddd
James Aimonetti <james@2600hz.com>
2019-03-25 13:12:52 -0700

    add some couch errors

--- klap/analyzers/klap-couch-mgr.sh
+++ klap/analyzers/klap-couch-mgr.sh
@@ -3,7 +3,7 @@
 . $(dirname $(dirname $0))/klap-utils.sh
 
 for FILE in $(listFiles $@); do
-	createTmpFile "couch_util"
+	createTmpFile "kz_couch_util"
 
 	isTmpFileEmpty
 	if [ $? != 0 ]; then
@@ -14,4 +14,12 @@ for FILE in $(listFiles $@); do
 
 	zgrep "unformatted error" $TMP_FILE | rev | cut -d " " -f 1 | rev | sort | uniq -c | sort -nr | head | printTable "Unformatted Errors"
         zgrep "gateway_timeout" $FILE | wc -l | printTable "Number of gateway timeouts"
+        zgrep "connection_timeout" $FILE | wc -l | printTable "Number of connection timeouts"
+        zgrep "network_unreachable" $FILE | wc -l | printTable "Number of network unreachables"
+        zgrep "tcp_closed" $FILE | wc -l | printTable "Number of Closed TCP sockets"
+        zgrep "system limit" $FILE | wc -l | printTable "Number of system limits met"
+        zgrep "econnrefused" $FILE | wc -l | printTable "Number of refused connections"
+        zgrep "server_timeout" $FILE | wc -l | printTable "Number of CouchDB timeouts"
+        zgrep "server error" $FILE | wc -l | printTable "Number of CouchDB errors"
+
 done
