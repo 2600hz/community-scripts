5a780d115d4d2227e0ca94ef0d2c95e30f6f57db
James Aimonetti <james@2600hz.com>
2019-03-27 14:55:57 -0700

    start an ecallmgr analyzer

---
+++ klap/analyzers/klap-ecallmgr.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+. $(dirname $(dirname $0))/klap-utils.sh
+
+for F in $(listFiles $@); do
+    FILE=$(realpath $F)
+    CWD=$(pwd)
+
+	createTmpFile "\|ecallmgr_"
+
+	isTmpFileEmpty
+	if [ $? != 0 ]; then
+		continue
+	fi
+
+        statHeader
+
+        printStat "Authorized channels" $(countMatches "channel is authorized$")
+
+        printStat "New FS Connections" $(countMatches "successfully connected to freeswitch node")
+        printStat "Lost FS Connections" $(countMatches "received node down notice")
+
+
+        grep -Ea "successfully connected to freeswitch node|received node down notice" $TMP_FILE | sed -re "s/.+(\w\w:\w\w:\w\w).*(connected|down).*freeswitch@([^\w]+)/\1 \2 \3/p" | sort | uniq | printTable "FreeSWITCH connections"
+
+        grep -a " updated registration " $TMP_FILE | grep -Po "@[\w\.]+" | sort | uniq -c | sort -nr | head | printTable "Registrations by realm"
+
+        grep -a " channel is authorized by account " $TMP_FILE | cut -d' ' -f13,15 | sort | uniq -c | sort -nr | head | printTable "Authz type by AccountId"
+        grep -a " channel is authorized by reseller " $TMP_FILE | cut -d' ' -f13,15 | sort | uniq -c | sort -nr | head | printTable "Authz type by ResellerId"
+
+        grep -Pa "ecallmgr_fs_authn.+user directory " $TMP_FILE | sed -re 's/.*freeswitch@([^|]+).*/\1/p' | sort | uniq -c | sort -nr | printTable "Authn Reqs by FreeSWITCH"
+        grep -Pa "ecallmgr_fs_route.+fetch request " $TMP_FILE | sed -re 's/.*freeswitch@([^|]+).*/\1/p' | sort | uniq -c | sort -nr | printTable "Route Reqs by FreeSWITCH"
+
+done
