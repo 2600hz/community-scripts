b77d9ce8987a6fb3ec0565e030352fea4f9efa96
James Aimonetti <james@2600hz.com>
2019-03-28 12:11:03 -0700

    more FS processing

--- klap/analyzers/klap-fs.sh
+++ klap/analyzers/klap-fs.sh
@@ -3,7 +3,7 @@
 . $(dirname $(dirname $0))/klap-utils.sh
 
 for FILE in $(listFiles $@); do
-	createTmpFile " mod_amqp"
+	createTmpFile " mod_amqp|kazoo_|mod_kazoo"
 
 	isTmpFileEmpty
 	if [ $? != 0 ]; then
@@ -13,4 +13,7 @@ for FILE in $(listFiles $@); do
         statHeader
 
         zgrep "failed to send heartbeat on connection" $TMP_FILE | grep -Po "([\s\w]+)\)?$" | uniq -c | sort -nr 2> /dev/null | head | printTable "Failed Heartbeat Reasons"
+        zgrep "Connected to epmd and published erlang cnode" $TMP_FILE | grep -Po " \d{2}:\d{2}:\d{2}" | head | printTable "KAZOO C-Node ready"
+        zgrep "opened socket connection to AMQP broker" $TMP_FILE | cut -d' ' -f2,12 | sort | uniq -c | head | printTable "Connections to AMQP"
+        zgrep "configuration name=\"acl.conf\"" $TMP_FILE | grep -Po " \d{2}:\d{2}:\d{2}" | uniq | printTable "ACL reloads"
 done
