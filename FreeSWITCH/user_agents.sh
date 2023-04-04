fa0c4f6a50fd38c337b734fd9daf71a5c6b5ae1b
Sergey <mentax@ya.ru>
2023-04-04 13:28:57 -0400

    Update FreeSwitch log file location
    Update FreeSwitch log file location from non-existing to existing correct location

--- FreeSWITCH/user_agents.sh
+++ FreeSWITCH/user_agents.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # show User-Agent breakdown
-# usage: ./user_agents.sh /var/log/freeswitch/debug.log
+# usage: ./user_agents.sh /var/log/freeswitch/kazoo-debug.log
 
 grep "User-Agent:" $1 | sort | uniq -c | sort -n
